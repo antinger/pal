3095247d2cb241b0c4fd968db90b432099279332
leon <3364802895@qq.com>
2020-03-24 12:38:46 +0800

    update

--- pal/src/main/java/com/pal/controller/PayController.java
+++ pal/src/main/java/com/pal/controller/PayController.java
@@ -1,8 +1,11 @@
 package com.pal.controller;
 
+import java.net.URLEncoder;
+import java.util.Enumeration;
 import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,10 +21,18 @@ public class PayController {
 	
 	@RequestMapping(path="/paysuccess/", method=RequestMethod.POST)
 	@ResponseBody
-	public String paysuccess(HttpServletRequest request) {
+	public String paysuccess(HttpServletRequest request, HttpServletResponse response) {
 		try {
 			System.out.println("支付成功---------");
-			System.out.println(JSON.toJSONString(request));
+			Enumeration<String> parameterNames = request.getParameterNames();
+			String str = "cmd=_notify-validate";
+			while (parameterNames.hasMoreElements()) {
+                String paramName = parameterNames.nextElement();
+                String paramValue = request.getParameter(paramName);
+                //此处的编码一定要和自己的网站编码一致，不然会出现乱码，paypal回复的通知为‘INVALID’
+                str = str + "&" + paramName + "=" + URLEncoder.encode(paramValue, "UTF-8");
+            }
+			System.out.println("paypal传递过来的交易信息:" + str);
 			Map<String, Object> map = null;
 			return PalUtils.toJSONString(200, map);
 		} catch (Exception e) {
