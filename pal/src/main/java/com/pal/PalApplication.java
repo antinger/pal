70852a89dd48b0dc0f16724c8ed16223b582cc6c
leon <3364802895@qq.com>
2020-04-25 10:24:08 +0800

    update

--- pal/src/main/java/com/pal/PalApplication.java
+++ pal/src/main/java/com/pal/PalApplication.java
@@ -2,9 +2,10 @@ package com.pal;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class PalApplication {
+public class PalApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PalApplication.class, args);
