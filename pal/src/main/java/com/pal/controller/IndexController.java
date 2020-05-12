8e7cbf9d2fc42ac44fdc4d151b052a99b190d41b
leon <3364802895@qq.com>
2020-05-12 19:56:21 +0800

    update

---
+++ pal/src/main/java/com/pal/controller/IndexController.java
@@ -0,0 +1,25 @@
+package com.pal.controller;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+	
+	@RequestMapping("/")
+    public String index(HttpServletRequest request) {
+		String userAgent = request.getHeader("user-agent");
+		// 判断属于哪类设备 pc
+		if (userAgent.contains("Windows") || userAgent.contains("Intel Mac")){
+			return "redirect:/pages/index.html";
+		}
+		// 手机
+		if (userAgent.contains("iPhone") || userAgent.contains("Android") || userAgent.contains("iPad")){
+			return "/h5/#/index/index";
+		}
+        return "";
+    }
+	
+}
--- pal/src/main/java/com/pal/utils/PalUtils.java
+++ pal/src/main/java/com/pal/utils/PalUtils.java
@@ -143,6 +143,4 @@ public class PalUtils {
 	    return ip;  
 	}
 	
-
-
 }
